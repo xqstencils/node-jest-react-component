4d90b1817ae38383e7c597681639be154308b11a
Cao Xiaoqi <xqcao@thoughtworks.com>
2018-11-10 23:32:02 +0800

    Change config

--- ci/release-npm.sh
+++ ci/release-npm.sh
@@ -4,7 +4,5 @@ set -e -u
 CURRENT_VERSION=`cat package.json | jq -r .version`
 
 echo 'Publish package to npm ...'
-yarn install
-yarn build
 yarn publish --new-version $CURRENT_VERSION --access public
 echo 'Publish package to npm done'
